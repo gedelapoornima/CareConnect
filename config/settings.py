8c5c97afa3db8ae7a797871d98f6958ab171cc28
gedela poornima <gedelapoornima2007@gmail.com>
2026-07-02 21:14:30 +0530

    Added SOS app

--- config/settings.py
+++ config/settings.py
@@ -40,10 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+    'drf_yasg',
+
     'users',
     'society',
     'emergency',
+    'sos',
     'notifications',
 ]
 
